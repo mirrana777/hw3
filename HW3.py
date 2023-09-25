2296872ea19ebb996d6efe141d7c78fd81efa743
Мира Крылова <krilova.mira@gmail.com>
2023-09-25 12:47:24 +0300

    initial changes

---
+++ HW3.py
@@ -0,0 +1,18 @@
+day = int(input("day: "))
+if day == 1:
+    print("mondey")
+elif day == 2:
+    print("tuesday")
+elif day == 3:
+    print("wednesday")
+elif day == 4:
+    print("thursday")
+elif day == 5:
+    print("friday")
+elif day ==6:
+    print("saturday")
+elif day == 7:
+    print("sunday")
+else:
+    print("day of week")
+
