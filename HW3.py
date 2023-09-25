8bdb2dcfb4a74288a692fe30e412356071d7aecc
Мира Крылова <krilova.mira@gmail.com>
2023-09-25 15:29:18 +0300

    initial changes

--- HW3.py
+++ HW3.py
@@ -16,3 +16,32 @@ elif day == 7:
 else:
     print("day of week")
 
+#task 2
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 == num2:
+    print("Equal numbers")
+elif num1 < num2:
+    print(num1, num2)
+elif num1 > num2:
+    print(num2, num1)
+else:
+    print("resault: ")
+
+#task3
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("\n1.+\n2.-\n3.* \n4. /")
+option = int(input("option: "))
+if option == 1:
+    print(num1 + num2 )
+elif option == 2:
+    print(num1 - num2)
+elif option == 3:
+    print(num1 * num2)
+elif option == 4:
+    print(num1 / num2)
+else:
+    print("answer")
+
+
